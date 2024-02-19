fefb16485e3f38d69c97f2de878791900e0dc13b
baptiste laisney <baptiste.laisney@gmail.com>
2024-02-19 16:01:13 +0100

    Add the affiche

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { horizontalTextAnimation } from './utils/functions.js';
 function updateAnimationMain(){
-    horizontalTextAnimation(220,500,'--title-banner-end-percentage','--title-banner-animation-duration-go-right')
+    horizontalTextAnimation(260,500,'--title-banner-end-percentage','--title-banner-animation-duration-go-right')
+    horizontalTextAnimation(1900,6000,'--title-after-end-percentage','--title-after-animation-duration-go-left')
 };
 
 window.addEventListener('resize', updateAnimationMain);
